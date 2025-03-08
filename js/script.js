ecc8b851543496f9accf530f4eef5d91e0197fc3
Leslie <les-lie@users.noreply.github.com>
2025-03-07 22:47:47 -0600

    Added zoom control to Google Maps

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function initMap() {
     const mapOptions = {
       center: { lat: 41.8781, lng: -87.6298 },
-      zoom: 12
+      zoom: 12,
+      zoomControl: true
     };
     const map = new google.maps.Map(document.getElementById('map'), mapOptions);
   
